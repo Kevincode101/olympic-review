ad37434081acc2aea6025c40076f304103401f1b
kevincode101 <kevin.lima.code@gmail.com>
2016-08-09 14:22:11 +0000

    finnish review

---
+++ olympic-medal-cont.rb
@@ -0,0 +1,41 @@
+  puts "U.S. Olympics Medal Count"
+
+
+  num_gold=5
+  num_silver=7
+  num_bronze=7
+
+total = num_gold + num_silver + num_bronze
+
+  puts"the U.S. has won"
+  puts "Gold  #{num_gold}"
+  puts "silver #{num_silver}"
+  puts "bronze " + num_bronze.to_s
+
+  puts "swimming".capitalize
+  puts "volleyball".upcase
+  puts "target shooing".downcase
+  puts "air rifle".reverse
+  puts "fencing".length
+  puts "diving".gsub("i","d")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
